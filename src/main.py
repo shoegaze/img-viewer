cea3d59dacea262a48905f9f0fe8ad06d16f9025
spike <bank.tellr@gmail.com>
2022-08-31 13:57:47 +0900

    Adds `tkinter` stub

--- src/main.py
+++ src/main.py
@@ -4,6 +4,18 @@ def main():
     im = Image.open("src/mando.jpg")
     print(im.format, im.size, im.mode)
 
+    from tkinter import Tk, ttk
+
+    root = Tk()
+
+    frame = ttk.Frame(root, padding=10)
+    frame.grid()
+
+    ttk.Label(frame, text="Hello, world!").grid(column=0, row=0)
+    ttk.Button(frame, text="Quit", command=root.destroy).grid(column=1, row=0)
+
+    root.mainloop()
+
 
 if __name__ == '__main__':
     main()
