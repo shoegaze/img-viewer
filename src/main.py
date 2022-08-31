21510d68aea5308114cc6fcab5169e76ce277e31
spike <bank.tellr@gmail.com>
2022-08-31 13:30:04 +0900

    Adds `Pillow` stub

--- src/main.py
+++ src/main.py
@@ -1,5 +1,9 @@
 def main():
-    print("Hello, world!")
+    from PIL import Image
+
+    im = Image.open("src/mando.jpg")
+    print(im.format, im.size, im.mode)
+
 
 if __name__ == '__main__':
     main()
